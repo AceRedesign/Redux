9629614205524fd40c298d4c5b38034dca38ff83
aceredesign <steven.magadan@hotmail.com>
2018-02-21 13:45:42 -0800

    added reducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -5,7 +5,8 @@ import { createStore } from "redux";
 import reducers from "./reducers";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(reducers);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
